a4123c62cf3bff88d175532a8c6125584bbb7c53
Andrew Pratt <apratt957@gmail.com>
2026-01-11 09:48:41 -0600

    add meta and main files

---
+++ _meta.lua
@@ -0,0 +1,6 @@
+local _ = require("gettext")
+return {
+    name = "betterstats",
+    fullname = _("Better Stats"),
+    description = _([[This plugin changes the way chapter and book progress are shown.]]),
+}
